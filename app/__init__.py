f83a002c41fc30f7fd49dcd1e5843f14e291054b
Yariyari <yaritza.urbina.sanhueza@gmail.com>
2022-12-05 11:19:11 -0300

    inicio de sesion

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,6 @@
 from flask import Flask
 from config import Config
-from extensions import db, migrate, login_manager
+from app.extensions import db, migrate, login_manager
 
 def create_app(config_class=Config):
     app = Flask(__name__)
@@ -13,14 +13,80 @@ def create_app(config_class=Config):
 
     # Register blueprints here
 
+    # auth blueprint
+    from app.auth import bp as auth_bp
+    app.register_blueprint(auth_bp, url_prefix='/auth')
+
+    # Memes blueprint
+    from app.memes import bp as memes_bp
+    app.register_blueprint(memes_bp, url_prefix='/memes')
     # Main blueprint
     from app.main import bp as main_bp
     app.register_blueprint(main_bp)
-    
     # Messages blueprint
     from app.messages import bp as messages_bp
     app.register_blueprint(messages_bp, url_prefix='/messages')
+    
+    @app.route('/test/')
+    def test_page():
+        return '<h1>Testing the Flask Application Factory Pattern</h1>'
 
+    return app
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+Enviar un mensaje a charlotte
+
+
+
+
+
+
+
+
+Mayús + Intro para añadir una nueva línea
+Slack necesita tu autorización para activar las notificaciones. Activar notificacionesfrom flask import Flask
+from config import Config
+from app.extensions import db, migrate, login_manager
+
+def create_app(config_class=Config):
+    app = Flask(__name__)
+    app.config.from_object(config_class)
+
+    # Initialize Flask extensions here
+    db.init_app(app)
+    migrate.init_app(app,db)
+    login_manager.init_app(app)
+
+    # Register blueprints here
+
+    # auth blueprint
+    from app.auth import bp as auth_bp
+    app.register_blueprint(auth_bp, url_prefix='/auth')
+
+    # Memes blueprint
+    from app.memes import bp as memes_bp
+    app.register_blueprint(memes_bp, url_prefix='/memes')
+    # Main blueprint
+    from app.main import bp as main_bp
+    app.register_blueprint(main_bp)
+    # Messages blueprint
+    from app.messages import bp as messages_bp
+    app.register_blueprint(messages_bp, url_prefix='/messages')
+    
     @app.route('/test/')
     def test_page():
         return '<h1>Testing the Flask Application Factory Pattern</h1>'
